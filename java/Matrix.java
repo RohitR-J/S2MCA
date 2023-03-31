22823bd4eaacb83847498417c6523e746f85d8e2
Rohit Raj <75240698+RohitR-J@users.noreply.github.com>
2023-03-31 12:26:02 +0530

    Add files via upload

---
+++ java/Matrix.java
@@ -0,0 +1,14 @@
+public class Matrix {
+    public static void main(String[] args){
+        int[][] matrix1 = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] matrix2 = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] matrix3 = new int[3][3];
+        for(int i = 0; i <3; i++){
+            for(int j = 0;j < 3;j++){
+                matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
+                System.out.print(matrix3[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
